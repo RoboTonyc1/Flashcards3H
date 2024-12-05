70efcf21fae5c48466fae8028d25a6fd61a2e52e
RoboTonyc1 <antoniopepinelli@professor.educacao.sp.gov.br>
2024-12-05 15:11:20 -0300

    Add files via upload

---
+++ pergunta.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Endocrinologia', /*categoria*/
+    'Qual hormônio tem efeito oposto à insulina?', /*pergunta*/
+    'O Glucagon.'/*resposta*/
+)
+
+criaCartao(
+    'Endocrinologia',
+    'Quais as funções do glucagon?',
+    'É responsável por quebrar as reservas nutritivas e aumentar a glicose sanguínea.'
+)
+
+criaCartao(
+    'Endocrinologia',
+    'Qual doença está associada à baixa produção de insulina?',
+    'Diabetes'
+)
